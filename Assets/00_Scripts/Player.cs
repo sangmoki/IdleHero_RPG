e696317b7abbe8e890b9537a46ac54a64bb9a546
sangmoki <yoochang4176@naver.com>
2024-11-18 11:12:20 +0900

    Feat: "근접 캐릭터의 공격 디테일 - Trail 구현하여 가시성 추가"

--- Assets/00_Scripts/Player.cs
+++ Assets/00_Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : Character
 {
     private Character_Scriptable CH_Data; // 캐릭터 데이터
+    public GameObject TrailObject;        // 플레이어의 공격을 표시할 Trail 오브젝트    
     public string CH_Name;                // 캐릭터 이름
     Vector3 startPos;                     // 플레이어의 시작 위치
     Quaternion rot;                       // 플레이어의 회전값
@@ -97,4 +98,14 @@ public class Player : Character
 
         HP -= dmg;
     }
+
+    protected override void Attack()
+    {
+        base.Attack();
+        TrailObject.SetActive(true);
+
+        Invoke("TrailDisable", 1.0f);   
+    }
+
+    private void TrailDisable() => TrailObject.SetActive(false);
 }
