970c6784562060314a57635a6d537068da883b09
sangmoki <yoochang4176@naver.com>
2024-11-25 12:31:38 +0900

    Feat: "FadeInOut 기능 구현 - Duration과 알파값 통한 내용으로 구현"

--- Assets/00_Scripts/UI/Main_UI.cs
+++ Assets/00_Scripts/UI/Main_UI.cs
@@ -1,14 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Main_UI : MonoBehaviour
 {
-    public static Main_UI instance = null; 
-
-    [SerializeField] private TextMeshProUGUI m_Level_Text;
-    [SerializeField] private TextMeshProUGUI m_AvgDPS_Text;
+    public static Main_UI instance = null;
 
     private void Awake()
     {
@@ -27,10 +26,58 @@ public class Main_UI : MonoBehaviour
         TextCheck();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            FadeInOut(true);
+            Debug.Log("Test");
+        } 
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            FadeInOut(false);
+            Debug.Log("Test22");
+        }
+    }
+
+    // 메인 UI 텍스트 변수
+    [SerializeField] private TextMeshProUGUI m_Level_Text;
+    [SerializeField] private TextMeshProUGUI m_AvgDPS_Text;
+
+    // 페이드 인아웃 작업 위한 변수
+    [SerializeField] private Image m_Fade;
+    [SerializeField] private float m_FadeDuration;
+
+    public void FadeInOut(bool FadeInOut)
+    {
+        StartCoroutine(FadeInOut_Coroutine(FadeInOut));
+    }
+
+    IEnumerator FadeInOut_Coroutine(bool FadeInOut, Action action = null)
+    {
+        float current = 0.0f;
+        float percent = 0.0f;
+        float start = FadeInOut ? 1.0f : 0.0f; // 1이면 255, 0이면 0
+        float end = FadeInOut ? 0.0f : 1.0f; // 1이면 0, 0이면 255
+        while (percent < 1.0f)
+        {
+            // fade 시간 조절
+            current += Time.deltaTime;
+            percent = current / m_FadeDuration;
+
+            // color 알파값 변경
+            float LerpPos = Mathf.Lerp(start, end, percent);
+            m_Fade.color = new Color(0, 0, 0, LerpPos);
+
+            yield return null;
+        }
+    }
+
     // 레벨업이 될 때마다 UI 상단 텍스트를 변경
     public void TextCheck()
     {
         m_Level_Text.text = "LV." + (Base_Mng.Player.Level + 1).ToString();
         m_AvgDPS_Text.text = StringMethod.ToCurrencyString(Base_Mng.Player.Average_DPS());
     }
+
 }
