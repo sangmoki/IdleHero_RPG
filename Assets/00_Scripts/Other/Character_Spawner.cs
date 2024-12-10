128c2c756ee3c7fbdea1a4390c7e22216ee96d81
sangmoki <yoochang4176@naver.com>
2024-12-10 16:16:00 +0900

    Feat: "영웅 탭에서 영웅 배치 후 준비 상태 시 영웅이 소환됨 추가"

--- Assets/00_Scripts/Other/Character_Spawner.cs
+++ Assets/00_Scripts/Other/Character_Spawner.cs
@@ -12,5 +12,22 @@ public class Character_Spawner : MonoBehaviour
         {
             SpawnTransform[i] = transform.GetChild(i);
         }
+
+        Stage_Manager.m_ReadyEvent += OnReady;
+    }
+
+    private void OnReady()
+    {
+        for (int i = 0; i < Base_Manager.Character.m_Set_Character.Length; i++)
+        {
+            var data = Base_Manager.Character.m_Set_Character[i];
+            if (data != null)
+            {
+                string temp = data.data.m_Character_Name;
+                var go = Instantiate(Resources.Load<GameObject>("Character/" + temp));
+                go.transform.position = SpawnTransform[i].position;
+                go.transform.LookAt(Vector3.zero);
+            }
+        }
     }
 }
