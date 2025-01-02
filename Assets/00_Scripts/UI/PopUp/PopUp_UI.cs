b83863ac654f9ccddb87622ae8729deda4bba056
sangmoki <yoochang4176@naver.com>
2025-01-02 12:58:20 +0900

    Feat: "아이템 정보를 담은 팝업 UI 구현 후 Resources화 및 컴포넌트 연결"

---
+++ Assets/00_Scripts/UI/PopUp/PopUp_UI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+// 아이템 설명 정보 팝업
+public class PopUp_UI : MonoBehaviour
+{
+    RectTransform rect;
+    [SerializeField] private Image IconImage;
+    [SerializeField] private TextMeshProUGUI TitleText, RarityText, ExplaneText;
+
+    private void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    public void Item_PopUp(Item_Scriptable item, Vector2 pos)
+    {
+        rect.anchoredPosition = pos;
+        IconImage.sprite = Utils.Get_Atlas(item.name);
+        TitleText.text = item.name;
+        RarityText.text = Utils.String_Color_Rarity(item.rarity) + " 등급</color>";
+        ExplaneText.text = item.Item_Des;
+    }
+}
