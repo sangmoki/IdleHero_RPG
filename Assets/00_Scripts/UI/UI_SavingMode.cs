d1b81f640036e870f2dcc623e1df16d6fbadc486
sangmoki <yoochang4176@naver.com>
2025-01-09 15:26:46 +0900

    Feat: "드래그를 통하여 절전모드 해제 기능 구현"

--- Assets/00_Scripts/UI/UI_SavingMode.cs
+++ Assets/00_Scripts/UI/UI_SavingMode.cs
@@ -7,12 +7,15 @@ using UnityEngine.UI;
 public class UI_SavingMode : UI_Base
 {
     [SerializeField] private TextMeshProUGUI BatteryText, TimerText, StageText, FightText;
-    [SerializeField] private Image BatteryFill;
+    [SerializeField] private Image BatteryFill, LandImage;
     [SerializeField] private Transform Content;
     [SerializeField] private UI_Inventory_Part item_part;
 
     public Dictionary<string, Item> m_SaveItem = new Dictionary<string, Item>();
     public Dictionary<string, UI_Inventory_Part> m_Parts = new Dictionary<string, UI_Inventory_Part>();
+
+    // 절전모드 해제 용도
+    Vector2 StartPos, EndPos;
 
     // Color(0.0f, 0.0f, 0.0f, 1.0f) -> 검정색
     Color m_Stage_Color = new Color(0, 0.7295136f, 1.0f, 1.0f);
@@ -29,9 +32,38 @@ public class UI_SavingMode : UI_Base
         int stageForward = (stageValue / 1000) + 1;
         int stageBack = stageValue % 1000;
 
-        StageText.text = "매우어려움 " + stageForward.ToString() + "-" + stageBack.ToString();
+        StageText.text = "보통 " + stageForward.ToString() + "-" + stageBack.ToString();
         FightText.text = Stage_Manager.isDead ? "반복중..." : "진행중...";
         FightText.color = Stage_Manager.isDead ? Color.yellow : m_Stage_Color;
+        
+        // 마우스가 한번 눌렸을 때
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartPos = Input.mousePosition;
+        }
+        // 마우스를 누르는 동안
+        if (Input.GetMouseButton(0))
+        {
+            // 마우스를 누르고 있는 동안 위치
+            EndPos = Input.mousePosition;
+            float distance = Vector2.Distance(EndPos, StartPos);
+
+            // 거리가 1500.0f 이상이면 비활성화
+            LandImage.color = new Color(1.0f, 1.0f, 1.0f, Mathf.Clamp(distance / (Screen.width / 2), 0.3f, 0.8f));
+            
+            if (distance >= Screen.width/2)
+            {
+                DisableOBJ();
+            }
+        }
+        // 마우스를 눌렀다가 뗀 순간
+        if (Input.GetMouseButtonUp(0))
+        {
+            StartPos = Vector2.zero;
+            EndPos = Vector2.zero;
+            LandImage.color = new Color(1.0f, 1.0f, 1.0f, 0.3f);
+        }
+
     }
 
     public void GetItem(Item_Scriptable item)
