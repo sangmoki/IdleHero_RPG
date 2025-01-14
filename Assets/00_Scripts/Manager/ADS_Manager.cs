ab8df451031c0c24cabafc6dc9c2c32ea2f55770
sangmoki <yoochang4176@naver.com>
2025-01-14 10:17:40 +0900

    Feat: "광고 추가 위한 Admob key id 저장"

---
+++ Assets/00_Scripts/Manager/ADS_Manager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ADS_Manager
+{
+    private bool TestMode = false;
+    public readonly string banner_Android_id = "ca-app-pub-1387955594217908/1960728208";
+    public readonly string interstitial_Android_id = "ca-app-pub-1387955594217908/2387237646";
+    public readonly string reward_Android_id = "ca-app-pub-1387955594217908/4462736647";
+
+}
